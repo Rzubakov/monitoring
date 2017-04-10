49c3d1c4569b0708915efbce29173f5a94fb1744
Rzubakov <rzubakov@protonmail.com>
2017-04-10 22:16:34 +0300

    Merge origin/master
    Conflicts:
    	README.md
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml

--- src/main/java/listeners/BaseListener.java
+++ src/main/java/listeners/BaseListener.java
@@ -9,9 +9,9 @@ public class BaseListener implements HttpSessionListener {
 	@Override
 	public void sessionCreated(HttpSessionEvent se) {
             
-		System.out.println("sessionCreated "+se.getSession().getServletContext().getContextPath());
+		System.out.println("SessionCreated "+se.getSession().getServletContext().getContextPath());
                 System.out.println(se.getSession().getServletContext().getServletContextName());
-	} 
+	}  
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
                 System.out.println(se.getSession().getAttribute("user"));
