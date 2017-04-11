42bef99faebf3e48f94910e34d835668fc998064
Rzubakov <rzubakov@protonmail.com>
2017-04-11 08:42:29 +0300

    Merge origin/master
    Conflicts:
    	README.md
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml

--- src/main/java/beans/CategoryBean.java
+++ src/main/java/beans/CategoryBean.java
@@ -79,8 +79,8 @@ public class CategoryBean implements Serializable {
 
     private void loadNodes(Category root, TreeNode node) {
         node.getChildren().clear();
-        categoryServiceJpa.getCategories(root).forEach((Category cat) -> {  
-            loadNodes(cat, new DefaultTreeNode(cat.toString()+itemServiceJpa.getCount(cat), node));
+        categoryServiceJpa.getCategories(root).forEach((Category cat) -> {
+            loadNodes(cat, new DefaultTreeNode(cat+"(t)", node));
         });
     }
 
