52c6f6516433aedb9f3aff933ee9c0ba73e59756
Rzubakov <rzubakov@protonmail.com>
2017-06-01 21:50:29 +0300

    Merge origin/master
    Conflicts:
    	README.md
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml

--- src/main/java/services/StandUpService.java
+++ src/main/java/services/StandUpService.java
@@ -1,5 +1,6 @@
 package services;
 
+
 import entitys.Category;
 import entitys.Role;
 import javax.annotation.PostConstruct;
@@ -9,17 +10,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import entitys.User;
-
 @Startup
 @Singleton
 public class StandUpService {
-
-    @PersistenceContext
-    protected EntityManager manager;
-
-    @PostConstruct
-    public void ini() {
-        /*	System.out.println("Create User");
+	@PersistenceContext
+	protected EntityManager manager;
+	
+	@PostConstruct
+	public void ini(){
+            	System.out.println("Create User");
 
                 User user = new User("rzubakov@protonmail.com","/gnplWM1A3X6t7sLuLjnOnqLZjAYewyEJLoq8+H6I4s=","Y", 10);
                 
@@ -32,6 +31,6 @@ public class StandUpService {
                 manager.persist(role);
 		manager.persist(user);
                 manager.persist(category);
-         */
-    }
+	 
+	}	
 }
