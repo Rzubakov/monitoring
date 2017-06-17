67dd763da71a26aec48106360cf362bde8f9c14a
Rzubakov <rzubakov@protonmail.com>
2017-06-17 21:28:58 +0300

    Merge origin/master
    Conflicts:
    	README.md
    	pom.xml
    	src/main/webapp/WEB-INF/web.xml

--- src/main/resources/META-INF/create.sql
+++ src/main/resources/META-INF/create.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (`email`, `password`, `role`) VALUES ('rzubakov@protonmail.com', '/gnplWM1A3X6t7sLuLjnOnqLZjAYewyEJLoq8+H6I4s=', 'ADMIN');
-INSERT INTO users (`email`, `password`, `role`) VALUES ('robot@bot', '/gnplWM1A3X6t7sLuLjnOnqLZjAYewyEJLoq8+H6I4s=', 'BOT');
-INSERT INTO categories (`description`, `name`) VALUES ('GlobalRoot', 'GlobalRoot');
-INSERT INTO categories (`description`, `name`, `parent_id`, `user_id`) VALUES ('Root', 'Root', '1', '1');
-INSERT INTO items (`name`, `path`, `url`, `category_id`, `user_id`) VALUES ('Test', 'Test', 'Test', '2', '1');
+INSERT INTO Users (`email`, `password`, `role`) VALUES ('rzubakov@protonmail.com', '/gnplWM1A3X6t7sLuLjnOnqLZjAYewyEJLoq8+H6I4s=', 'ADMIN');
+INSERT INTO Users (`email`, `password`, `role`) VALUES ('robot@bot', '/gnplWM1A3X6t7sLuLjnOnqLZjAYewyEJLoq8+H6I4s=', 'BOT');
+INSERT INTO Categories (`description`, `name`) VALUES ('GlobalRoot', 'GlobalRoot');
+INSERT INTO Categories (`description`, `name`, `parent_id`, `user_id`) VALUES ('Root', 'Root', '1', '1');
+INSERT INTO Items (`name`, `path`, `url`, `category_id`, `user_id`) VALUES ('Test', 'Test', 'Test', '2', '1');
  
